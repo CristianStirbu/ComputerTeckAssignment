a3a1fa6841ded3274c4dcefe29b68797438471d0
CristianStirbu <cristian.stirbu@edu.futuregames.nu>
2024-09-08 22:47:35 +0200

    things

--- Assets/Scrpits/PlayerAuthoring.cs
+++ Assets/Scrpits/PlayerAuthoring.cs
@@ -1,5 +1,7 @@
 
+using System.ComponentModel;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerAuthoring : MonoBehaviour
@@ -13,6 +15,46 @@ public class PlayerAuthoring : MonoBehaviour
         public override void Bake(PlayerAuthoring authoring)
         {
             Entity playerEntiy = GetEntity(TransformUsageFlags.Dynamic);
+
+            AddComponent<PlayerTag>(playerEntiy);
+            AddComponent<PlayerMoveInput>(playerEntiy);
+
+            AddComponent(playerEntiy, new PlayerMoveSpeed { Value = authoring.speed });
+
+            AddComponent<FireProjectileTag>(playerEntiy);
+            SetComponentEnabled<FireProjectileTag>(playerEntiy, false);
+
+            //AddComponent(playerEntiy, new ProjectillePrefab { Value = GetEntity(authoring.ProjectilePrefab,TransformUsageFlags.Dynamic)});
         }
     }
+}
+
+public struct PlayerMoveInput: IComponentData
+{
+    public float2 Value;
+}
+
+public struct PlayerMoveSpeed: IComponentData
+{
+    public float Value;
+}
+
+public struct PlayerTag : IComponentData
+{
+
+}
+
+public struct ProjectillePrefab : IComponentData
+{
+    public float Value;
+}
+
+public struct ProjectileMoveSpeed : IComponentData
+{
+    public float Value;
+}
+
+public struct FireProjectileTag : IComponentData, IEnableableComponent
+{
+
 }
