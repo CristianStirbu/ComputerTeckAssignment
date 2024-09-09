1dc274c5ddfc12b7f9858c067c08b0ae56c99926
CristianStirbu <145765962+CristianStirbu@users.noreply.github.com>
2024-09-09 16:15:37 +0200

    new stuff

---
+++ Assets/Scrpits/InputComponent.cs
@@ -0,0 +1,11 @@
+
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct InputComponent : IComponentData
+{
+    public float2 movement;
+    public float2 mousePos;
+    public bool pressingLMB; 
+}
